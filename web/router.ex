017fff5c105c8494f36c01d6e4b57e00eb72cead
Chris McCord <chris@chrismccord.com>
2014-08-19 18:50:14 -0400

    Remove unnecessary static asset plug
    Plug.Static is plugged automatically when `state_assets: true`

--- web/router.ex
+++ web/router.ex
@@ -1,7 +1,6 @@
 defmodule BeamToolboxWeb.Router do
   use Phoenix.Router
 
-  plug Plug.Static, at: "/static", from: :beam_toolbox_web
   get "/", BeamToolboxWeb.PageController, :index, as: :page
 
   scope alias: BeamToolboxWeb do
